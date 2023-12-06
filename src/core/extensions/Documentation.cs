e3404d524dbec7f3778ec1dba5df2eb35507993e
Braden Franksen <38736705+bfranksen@users.noreply.github.com>
2023-12-05 19:59:50 -0800

    More restructuring, to be more consistent

--- src/core/extensions/Documentation.cs
+++ src/core/extensions/Documentation.cs
@@ -1,11 +1,11 @@
 ﻿namespace BeeneticToolkit {
 
     namespace Extensions {
+
         /// <summary>
         /// The <see cref="Extensions"/> namespace contains a collection of custom extension methods designed to enhance and simplify various programming tasks in C#.
         /// This may include numeric type conversions, string manipulations, data structure enhancements, and other utility functions that are frequently needed in various development scenarios.
         /// </summary>
-
         internal static class NamespaceDoc { }
     }
 }
